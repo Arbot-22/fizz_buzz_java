bf5ca5c0fb4df96be135dae590c4e5a2cee5dd87
Arbot-22 <arbabacar@gmail.com>
2022-02-10 23:31:30 +0000

    tests for FizzBuzz() method implemented

--- src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
+++ src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.fizz_buzz;
 
 import org.junit.jupiter.api.Test;
+import com.example.fizz_buzz.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.Serial;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FizzBuzzApplicationTests {
@@ -9,5 +14,14 @@ class FizzBuzzApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void testFizzBuzz(){
+		assertEquals("1", new FizzBuzzApplication().FizzBuzz(1)) ;
+		assertEquals("Fizz", new FizzBuzzApplication().FizzBuzz(3));
+		assertEquals("Buzz", new FizzBuzzApplication().FizzBuzz(5));
+		assertEquals("FizzBuzz", new FizzBuzzApplication().FizzBuzz(15));
+	}
+
 
 }
