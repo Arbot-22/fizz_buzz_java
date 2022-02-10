534ba2fac6efcb859ad93f0fb1381cbfcb19b70c
Arbot-22 <arbabacar@gmail.com>
2022-02-10 23:53:04 +0000

    test for FizzBuzz2() method implemented

--- src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
+++ src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
@@ -6,21 +6,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.Serial;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FizzBuzzApplicationTests {
-
+	FizzBuzzApplication fizzBuzz = new FizzBuzzApplication();
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
 	void testFizzBuzz(){
-		assertEquals("1", new FizzBuzzApplication().FizzBuzz(1)) ;
-		assertEquals("Fizz", new FizzBuzzApplication().FizzBuzz(3));
-		assertEquals("Buzz", new FizzBuzzApplication().FizzBuzz(5));
-		assertEquals("FizzBuzz", new FizzBuzzApplication().FizzBuzz(15));
+		assertEquals("1", fizzBuzz.FizzBuzz(1)) ;
+		assertEquals("Fizz", fizzBuzz.FizzBuzz(3));
+		assertEquals("Buzz", fizzBuzz.FizzBuzz(5));
+		assertEquals("FizzBuzz", fizzBuzz.FizzBuzz(15));
+	}
+
+	@Test
+	void testFizzBuzz2(){
+		assertEquals("1", fizzBuzz.FizzBuzz2(1));
+		assertEquals("Fizz", fizzBuzz.FizzBuzz2(3));
+		assertEquals("Fizz", fizzBuzz.FizzBuzz2(38));
+		assertEquals("Buzz", fizzBuzz.FizzBuzz2(5));
+		assertEquals("Buzz", fizzBuzz.FizzBuzz2(58));
+		assertEquals("Buzz", fizzBuzz.FizzBuzz2(15));
+
+
 	}
 
 
