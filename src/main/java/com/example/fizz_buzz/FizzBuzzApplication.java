f9fc540bd52b44c4a1f5b66b0ee777334d098955
Arbot-22 <arbabacar@gmail.com>
2022-02-10 23:25:35 +0000

    FizzBuzz() method implemented

--- src/main/java/com/example/fizz_buzz/FizzBuzzApplication.java
+++ src/main/java/com/example/fizz_buzz/FizzBuzzApplication.java
@@ -5,7 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FizzBuzzApplication {
-
+	public String FizzBuzz(int number){
+		if(number % 15 == 0){
+			return "FizzBuzz";
+		}else if (number % 3 == 0){
+			return "Fizz";
+		}else if (number % 5 == 0){
+			return "Buzz";
+		}else{
+			return  Integer.toString(number) ;
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FizzBuzzApplication.class, args);
 	}
