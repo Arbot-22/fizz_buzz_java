4518b652e7dcccd01add4129c3728d987bd20720
Arbot-22 <arbabacar@gmail.com>
2022-02-11 01:19:27 +0000

    Methods createFile() and insertValue(String result) are implemented

--- src/main/java/com/example/fizz_buzz/FizzBuzzApplication.java
+++ src/main/java/com/example/fizz_buzz/FizzBuzzApplication.java
@@ -3,6 +3,13 @@ package com.example.fizz_buzz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 @SpringBootApplication
 public class FizzBuzzApplication {
 	public static String fizzBuzz(int number) {
@@ -29,10 +36,35 @@ public class FizzBuzzApplication {
 			return Integer.toString(number); //if the number is not divisible by 15 or 5 or by 3 his value is returned
 		}
 	}
+	public static void createFile(){
+		try {
+			File file = new File("fizzBuzz.txt");
+			if (file.createNewFile()) {
+				System.out.println("File created: " + file.getName());
+				System.out.println("Path created: " + file.getPath());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
 
+	public static void insertValue(String result){
+		result= result + "\n";
+		try {
+			Path path = Paths.get("./fizzBuzz.txt");
+			Files.write(path, result.getBytes() , StandardOpenOption.APPEND);
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+	}
 	public static void main(String[] args){
 		SpringApplication.run(FizzBuzzApplication.class, args);
-
 
 	}
 }
