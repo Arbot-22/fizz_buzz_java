f584e47d2a81a66d3b7277b679b4e8d0cde3c728
Arbot-22 <arbabacar@gmail.com>
2022-02-11 01:17:30 +0000

    Refactored fizzBuzz() and fizzBuzz2() as static methods

--- src/main/java/com/example/fizz_buzz/FizzBuzzApplication.java
+++ src/main/java/com/example/fizz_buzz/FizzBuzzApplication.java
@@ -5,31 +5,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FizzBuzzApplication {
-	public String FizzBuzz(int number){
-		if(number % 15 == 0){
-			return "FizzBuzz";
-		}else if (number % 3 == 0){
-			return "Fizz";
-		}else if (number % 5 == 0){
-			return "Buzz";
-		}else{
-			return  Integer.toString(number) ;
+	public static String fizzBuzz(int number) {
+		if (number % 15 == 0) {
+			return "FizzBuzz"; //if the number is divisible by 5 and 3 (by 15) FizzBuzz is returned
+		} else if (number % 3 == 0) {
+			return "Fizz"; //if the number is divisible by 3 Fizz is returned
+		} else if (number % 5 == 0) {
+			return "Buzz"; //if the number is divisible by 5 Buzz is returned
+		} else {
+			return Integer.toString(number); //if the number is not divisible by 15 or 5 or by 3 his value is returned
 		}
 	}
-	public String FizzBuzz2(int number){
 
-		if(number % 15 == 0){
-			return "FizzBuzz";
-		}else if (number % 3 == 0 || Integer.toString(number).contains("3")){
-			return "Fizz";
-		}else if (number % 5 == 0 ||  Integer.toString(number).contains("5")){
-			return "Buzz";
-		}else{
-			return  Integer.toString(number) ;
+	public static String fizzBuzz2(int number) {
+
+		if (number % 15 == 0) {
+			return "FizzBuzz"; //if the number is divisible by 5 and 3 (by 15) FizzBuzz is returned
+		} else if (number % 3 == 0 || Integer.toString(number).contains("3")) {
+			return "Fizz"; //if the number is divisible by 3 or contains 3 Fizz is returned
+		} else if (number % 5 == 0 || Integer.toString(number).contains("5")) {
+			return "Buzz"; //if the number is divisible by 5 or contains 5 Fizz is returned
+		} else {
+			return Integer.toString(number); //if the number is not divisible by 15 or 5 or by 3 his value is returned
 		}
 	}
-	public static void main(String[] args) {
+
+	public static void main(String[] args){
 		SpringApplication.run(FizzBuzzApplication.class, args);
-	}
 
+
+	}
 }
--- src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
+++ src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
@@ -1,37 +1,35 @@
 package com.example.fizz_buzz;
 
 import org.junit.jupiter.api.Test;
-import com.example.fizz_buzz.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.Serial;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static com.example.fizz_buzz.FizzBuzzApplication.fizzBuzz;
+import static com.example.fizz_buzz.FizzBuzzApplication.fizzBuzz2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FizzBuzzApplicationTests {
-	FizzBuzzApplication fizzBuzz = new FizzBuzzApplication();
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
 	void testFizzBuzz(){
-		assertEquals("1", fizzBuzz.FizzBuzz(1)) ;
-		assertEquals("Fizz", fizzBuzz.FizzBuzz(3));
-		assertEquals("Buzz", fizzBuzz.FizzBuzz(5));
-		assertEquals("FizzBuzz", fizzBuzz.FizzBuzz(15));
+		assertEquals("1", fizzBuzz(1)) ;
+		assertEquals("Fizz", fizzBuzz(3));
+		assertEquals("Buzz", fizzBuzz(5));
+		assertEquals("FizzBuzz", fizzBuzz(15));
 	}
 
 	@Test
 	void testFizzBuzz2(){
-		assertEquals("1", fizzBuzz.FizzBuzz2(1));
-		assertEquals("Fizz", fizzBuzz.FizzBuzz2(3));
-		assertEquals("Fizz", fizzBuzz.FizzBuzz2(38));
-		assertEquals("Buzz", fizzBuzz.FizzBuzz2(5));
-		assertEquals("Buzz", fizzBuzz.FizzBuzz2(58));
-		assertEquals("Buzz", fizzBuzz.FizzBuzz2(15));
+		assertEquals("1", fizzBuzz2(1));
+		assertEquals("Fizz", fizzBuzz2(3));
+		assertEquals("Fizz", fizzBuzz2(38));
+		assertEquals("Buzz", fizzBuzz2(5));
+		assertEquals("Buzz", fizzBuzz2(58));
+		assertEquals("Buzz", fizzBuzz2(15));
 
 
 	}
