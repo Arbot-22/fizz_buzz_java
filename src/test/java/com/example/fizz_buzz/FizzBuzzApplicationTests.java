90df7b2ed577eb84c296c05826f7555d7d6eba60
Arbot-22 <arbabacar@gmail.com>
2022-02-11 02:13:12 +0000

    Refactored tests using BDD

--- src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
+++ src/test/java/com/example/fizz_buzz/FizzBuzzApplicationTests.java
@@ -10,29 +10,137 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class FizzBuzzApplicationTests {
 
+	/*
+	* Tests for the method fizzBuzz()
+	* Testing the return value of a given integer param 'number'*/
+
 	@Test
-	void contextLoads() {
+	void shouldReturn1(){
+		//Given
+		int number = 1;
+
+		//When
+		String result = fizzBuzz(number);
+
+		//Then
+		String expected ="1";
+		assertEquals(expected,result);
 	}
 
 	@Test
-	void testFizzBuzz(){
-		assertEquals("1", fizzBuzz(1)) ;
-		assertEquals("Fizz", fizzBuzz(3));
-		assertEquals("Buzz", fizzBuzz(5));
-		assertEquals("FizzBuzz", fizzBuzz(15));
+	void shouldReturnFizz(){
+		//Given
+		int number = 3;
+
+		//When
+		String result = fizzBuzz(number);
+
+		//Then
+		String expected ="Fizz";
+		assertEquals(expected,result);
+	}
+	@Test
+	void shouldReturnBuzz(){
+		//Given
+		int number = 5;
+
+		//When
+		String result = fizzBuzz(number);
+
+		//Then
+		String expected ="Buzz";
+		assertEquals(expected,result);
 	}
 
 	@Test
-	void testFizzBuzz2(){
-		assertEquals("1", fizzBuzz2(1));
-		assertEquals("Fizz", fizzBuzz2(3));
-		assertEquals("Fizz", fizzBuzz2(38));
-		assertEquals("Buzz", fizzBuzz2(5));
-		assertEquals("Buzz", fizzBuzz2(58));
-		assertEquals("Buzz", fizzBuzz2(15));
+	void shouldReturnFizzBuzz(){
+		//Given
+		int number = 30;
 
+		//When
+		String result = fizzBuzz(number);
 
+		//Then
+		String expected ="FizzBuzz";
+		assertEquals(expected,result);
 	}
 
+	/*
+	 * Tests for the method fizzBuzz2()
+	 * Testing the return value of a given integer param 'number'*/
 
+	@Test
+	void shouldReturn1Stage2(){
+		//Given
+		int number = 1;
+
+		//When
+		String result = fizzBuzz2(number);
+
+		//Then
+		String expected ="1";
+		assertEquals(expected,result);
+	}
+
+	@Test
+	void shouldReturnFizzStage2(){
+		//Given
+		int number = 3;
+
+		//When
+		String result = fizzBuzz2(number);
+
+		//Then
+		String expected ="Fizz";
+		assertEquals(expected,result);
+	}
+	@Test
+	void shouldReturnFizzStage2Bis(){
+		//Given
+		int number = 37;
+
+		//When
+		String result = fizzBuzz2(number);
+
+		//Then
+		String expected ="Fizz";
+		assertEquals(expected,result);
+	}
+	@Test
+	void shouldReturnBuzzStage2(){
+		//Given
+		int number = 5;
+
+		//When
+		String result = fizzBuzz2(number);
+
+		//Then
+		String expected ="Buzz";
+		assertEquals(expected,result);
+	}
+	@Test
+	void shouldReturnBuzzStage2Bis(){
+		//Given
+		int number = 58;
+
+		//When
+		String result = fizzBuzz2(number);
+
+		//Then
+		String expected ="Buzz";
+		assertEquals(expected,result);
+	}
+
+	@Test
+	void shouldReturnFizzBuzzStage2(){
+		//Given
+		int number = 15;
+
+		//When
+		String result = fizzBuzz2(number);
+
+		//Then
+		String expected ="FizzBuzz";
+		assertEquals(expected,result);
+	}
 }
